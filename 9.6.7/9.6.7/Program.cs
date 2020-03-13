1022ecf06757ede88a8335b0f1758c06e73071a7
EliseevVadim <55801514+EliseevVadim@users.noreply.github.com>
2020-03-13 11:06:23 +0300

    T6V7

---
+++ 9.6.7/9.6.7/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _9._6._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDecision();
+            LinkedListDecision();
+        }
+        static void ListDecision()
+        {
+            Random rand = new Random();
+            List<int> firstlist = new List<int>();
+            List<int> secondlist = new List<int>();
+            int position = rand.Next(5, 15);
+            for (int i = 0; i < position; i++)
+            {
+                firstlist.Add(rand.Next(0, 50));
+                secondlist.Add(rand.Next(0, 50));
+            }
+            Console.WriteLine("List1: ");
+            for (int i = 0; i < firstlist.Count; i++)
+            {
+                Console.Write(firstlist[i] + " ");
+            }
+            Console.WriteLine("\nList2: ");
+            for (int i = 0; i < secondlist.Count; i++)
+            {
+                Console.Write(secondlist[i] + " ");
+            }
+            for (int i=secondlist.Count-1; i>=0; i--)
+            {
+                firstlist.Add(secondlist[i]);
+            }
+            Console.WriteLine("\nModified List1: ");
+            for (int i=0;i<firstlist.Count; i++)
+            {
+                Console.Write(firstlist[i]+" ");
+            }            
+        }
+        static void LinkedListDecision()
+        {
+            DoubleLinkedList list1 = new DoubleLinkedList();
+            DoubleLinkedList list2 = new DoubleLinkedList();
+            Random random = new Random();
+            int pos = random.Next(5, 15);
+            for (int i=0; i<pos; i++)
+            {
+                list1.AddElement(random.Next(0, 50));
+                list2.AddElement(random.Next(0, 50));
+            }
+            Console.WriteLine("\nLinkedList1: ");
+            list1.PrintList();
+            Console.WriteLine("\nLinkedList2: ");
+            list2.PrintList();
+            Console.WriteLine("\nModified LinkedList1: ");
+            int[] array = list2.ElementsFromEndToArray();
+            for (int i=0; i<array.Length; i++)
+            {
+                list1.AddElement(array[i]);
+            }
+            list1.PrintList();
+        }
+    }
+}
---
+++ 9.6.7/9.6.7/SingleLinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9._6._7
+{
+    class ListNode
+    {
+        public int data;
+        public ListNode next;
+        public ListNode prev;
+    }
+    public class DoubleLinkedList
+    {
+        ListNode head = null;
+        ListNode tail = null;
+        public void AddElement(int element)
+        {
+            ListNode node = new ListNode();
+            node.data = element;
+            node.next = null;
+            node.prev = null;
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                tail.next = node;
+                node.prev = tail;
+            }
+            tail = node;
+        }
+        public void PrintList()
+        {
+            ListNode node = head;
+            while (node != null)
+            {
+                Console.Write(node.data + " ");
+                node = node.next;
+            }
+        }        
+        public int Length()
+        {
+            int length = 0;
+            for (ListNode node = head; node != null; node = node.next, length++) ;
+            return length;
+        }
+        public int [] ElementsFromEndToArray()
+        {
+            ListNode node = tail;
+            int[] arr = new int[Length()];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = node.data;
+                node = node.prev;
+            }
+            return arr;
+        }
+    }    
+}
