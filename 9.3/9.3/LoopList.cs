acd6ebaaa64f67ceb851047be70b83b728f29db3
EliseevVadim <55801514+EliseevVadim@users.noreply.github.com>
2020-03-13 15:14:54 +0300

    T3

---
+++ 9.3/9.3/LoopList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9._3
+{
+    class ListNode
+    {
+        public string data;
+        public ListNode next;
+    }
+    public class LoopList
+    {
+        ListNode head = null;
+        ListNode tail = null;
+        public void Add(string name)
+        {
+            ListNode node = new ListNode();
+            node.data = name;
+            node.next = null;
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+                tail.next = head;
+            }
+            else
+            {
+                node.next = head;
+                tail.next = node;
+                tail = node;
+            }
+        }
+        public void Print()
+        {
+            ListNode node = head;
+            while (node != tail)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+            Console.WriteLine(tail.data);
+        }
+        public void Counting (string s, int num)
+        {
+            ListNode node = new ListNode();
+            node = head;
+            ListNode start;
+            while (node != tail)
+            {
+                if (node.data == s)
+                {
+                    start = node;
+                    goto linker1;                    
+                }
+                else
+                {
+                    node = node.next;
+                }
+            }
+            if (tail.data == s)
+            {
+                start = tail;
+            }
+            else
+            {
+                Console.WriteLine("Проверьте входные данные");
+                return;
+            }
+            linker1:
+            for (int i=1; i<num; i++)
+            {
+                start = start.next;
+                if (i == num-1)
+                {
+                    Console.WriteLine(start.data);
+                }
+            }
+        }
+        
+    }
+}
---
+++ 9.3/9.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+namespace _9._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите считалочку: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Введите имя человека с которого начнем: ");
+            string name = Console.ReadLine();
+            string[] countingarr = GetCountingArray(input);            
+            string[] names = InitNamesArray();
+            LoopList list = LoopListInit(names);            
+            Console.WriteLine("Выходит");
+            list.Counting(name, countingarr.Length);
+        }
+        static string [] InitNamesArray()
+        {
+            StreamReader reader = File.OpenText(@"d:\L9\9.3\names.txt");
+            string[] names = new string[10];
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = reader.ReadLine();
+            }
+            reader.Close();
+            return names;            
+        }
+        static string [] GetCountingArray(string s)
+        {
+            string [] arr=s.Split(new char[] {' ' });
+            return arr;
+        }
+        static LoopList LoopListInit(string [] arr)
+        {
+            LoopList list = new LoopList();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                list.Add(arr[i]);
+            }
+            return list; 
+        }
+    }
+}
