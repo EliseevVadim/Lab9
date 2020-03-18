047cbd4b3d9788951191270036273c66c8ecdb80
EliseevVadim <55801514+EliseevVadim@users.noreply.github.com>
2020-03-18 10:04:09 +0300

    T2

---
+++ 9.2/9.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _9._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>();
+            Console.WriteLine("Введите выражение");
+            string s = Console.ReadLine();
+            try
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == '(')
+                    {
+                        stack.Push(s[i]);
+                    }
+                    if (s[i] == ')')
+                    {
+                        stack.Pop();
+                    }
+                }
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine("Выражение корректно");
+                }
+                else
+                {
+                    Console.WriteLine("Выражение некорректно");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Выражение некорректно");
+            }
+        }
+    }
+}
